f054268420b34567b861dab4d94aa1ab3b12e8c5
Mikeford79 <bigmikeford1979@gmail.com>
2025-01-03 13:35:36 -0500

    Create test_gui.go

---
+++ test_gui.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "github.com/andlabs/ui"
+    _ "github.com/andlabs/ui/winmanifest"
+)
+
+func main() {
+    err := ui.Main(func() {
+        window := ui.NewWindow("Test GUI", 300, 100, true)
+        label := ui.NewLabel("Hello, world!")
+        window.SetChild(label)
+        window.OnClosing(func(*ui.Window) bool {
+            ui.Quit()
+            return true
+        })
+        window.Show()
+    })
+    if err != nil {
+        panic(err)
+    }
+}
